d07d294382701964a866d83854576e1c17deb15e
Tony-yzj <1634644151@qq.com>
2023-05-06 19:26:53 +0800

    With basic funcdef & exp support (error-corrected)

--- sysy-make-template/src/main.cpp
+++ sysy-make-template/src/main.cpp
@@ -13,7 +13,7 @@
 using namespace std;
 
 extern FILE *yyin;
-extern int yyparse(unique_ptr<BaseAST> &ast);
+extern int yyparse(BaseAST* &ast);
 
 int main(int argc, const char *argv[]) {
   // 解析命令行参数. 测试脚本/评测平台要求你的编译器能接收如下参数:
@@ -31,7 +31,7 @@ int main(int argc, const char *argv[]) {
   assert(yyin);
 
   // 调用 parser 函数, parser 函数会进一步调用 lexer 解析输入文件的
-  unique_ptr<BaseAST> ast;
+  BaseAST* ast;
   auto ret = yyparse(ast);
   assert(!ret);
 
@@ -41,7 +41,7 @@ int main(int argc, const char *argv[]) {
 
   // generate koopa IR
   Program p;
-  unique_ptr<std::string> ir_str = p.Scan(ast.get());
+  std::string* ir_str = p.Scan(ast);
   FILE *IRout;
   IRout = fopen(output, "w");
   fputs((*ir_str).c_str(), IRout);
